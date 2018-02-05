7c5008bd8ced1555c715a0a1fc04cf2cf6e2ea47
Marcos Macedo <Marcos Macedo@OM30NBRH98KVVC>
2018-02-05 10:52:31 -0200

    Inclusão forms parametros

---
+++ NovoIntegraInterface/Documento/frmConsultaParametros.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace NovoIntegraInterface.Documento
+{
+    partial class frmConsultaParametros
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmConsultaParametros
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 261);
+            this.Name = "frmConsultaParametros";
+            this.Text = "frmConsultaParametros";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
